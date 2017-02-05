94ee3e8ceb53e60892da5d5c10efca77e94a9558
VToropov <vladimirtoropov@MacBook-Air-Vladimir.local>
2017-02-05 22:26:15 +0300

    deleted 1 pizza

--- db/migrate/20170205130102_add_product.rb
+++ db/migrate/20170205130102_add_product.rb
@@ -1,11 +1,11 @@
-class AddProducts < ActiveRecord::Migration[5.0]
-  def change
+class AddProduct < ActiveRecord::Migration[5.0]
+def change
 
 	Product.create :title => 'Hawaiian', :description => 'This is just hawaiian pizza', :price => 350, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => false, :path_to_image => '/images/hawaii.jpg'
 
   	 Product.create :title =>'Pepperoni', :description => 'Nice pepperoni pizza', :price => 450, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => true, :path_to_image => '/images/pepper.jpg'
 
-  	Product.create :title => 'Vegetarian', :description => 'Amazing vagetarian pizza', :price => 400, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => false, :path_to_image => '/images/veg.png'
+  
 
   end
 end
--- public/vendor/app.js
+++ public/vendor/app.js
@@ -2,11 +2,11 @@
 
 function something()
 {
-	var x = window.localStorage.getItem('bbb');
+	var x = window.localStorage.getItem('bbb'); // структура данных локалстрадж это хэш, следовательно это равносилно конструкции x = hh['bbb'] 
 
-	x = x * 1 + 1
+	x = x * 1 + 1 // x = x +1 ( in ruby)
 
-	window.localStorage.setItem('bbb', x);
+	window.localStorage.setItem('bbb', x); //  hh['bbb'] = x
 
 	alert(x);
 
