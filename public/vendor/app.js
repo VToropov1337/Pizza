6585b84bb9d4cb0f3efd721fdebfb504f0e87d69
VToropov <vladimirtoropov@MacBook-Air-Vladimir.local>
2017-02-10 01:16:27 +0300

    add cart

--- public/vendor/app.js
+++ public/vendor/app.js
@@ -1,38 +1,39 @@
-
-
 function something()
 {
-	var x = window.localStorage.getItem('bbb'); // структура данных локалстoрадж это хэш, следовательно это равносилно конструкции x = hh['bbb'] 
+	var x = window.localStorage.getItem('bbb');
 
-	x = x * 1 + 1 // x = x +1 ( in ruby)
+	x = x * 1 + 1
 
-	window.localStorage.setItem('bbb', x); //  hh['bbb'] = x
+	window.localStorage.setItem('bbb',x);
 
 	alert(x);
 }
 
+
 function add_to_cart(id)
 {
-	var key = 'product_' + id
+	var key = 'product_' + id;
+
 	var x = window.localStorage.getItem(key);
-
-	x = x * 1 + 1
-	window.localStorage.setItem(key,x);
-
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x);
+	// вывод кол-во в корзине
+	alert('Items in your cart: ' + cart_get_number_of_items());
 }
 
-function cart_get_items_of_items()
+
+function cart_get_number_of_items()
 {
-	for(var i = 0; < window.localStorage.length; i++)
+	var cnt = 0
+	for(var i = 0; i < window.localStorage.length; i++)
 	{
-		var key = window.localStorage.key(i); // получаем ключ
-		var value = window.localStorage.getItem(key); //получаем значение, аналог в руби hh[key] = x 
+			var key = window.localStorage.key(i); // получаем ключ
+			var value = window.localStorage.getItem(key); // получаем значение по ключу
 
-
-
+			if (key.indexOf('product_') == 0) 
+			{
+					cnt = cnt + value * 1;
+			}
 	}
-
-
+	return cnt;
 }
-
-
