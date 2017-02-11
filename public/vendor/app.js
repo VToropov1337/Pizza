c4af7d97cc25978497999104117b0f3de01dd08b
VToropov <vladimirtoropov@MacBook-Air-Vladimir.local>
2017-02-11 17:44:05 +0300

    added cart

--- public/vendor/app.js
+++ public/vendor/app.js
@@ -18,9 +18,24 @@ function add_to_cart(id)
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
 	// вывод кол-во в корзине
-	alert('Items in your cart: ' + cart_get_number_of_items());
+	//alert('Items in your cart: ' + cart_get_number_of_items());
+
+	update_orders_input();
+	update_orders_button(); 
 }
 
+function update_orders_input()
+{
+	var orders = cart_get_orders();
+	$('#orders_input').val(orders);
+}
+
+function update_orders_button()
+	{
+		var text = 'Cart (' + cart_get_number_of_items() + ')';
+		$('#orders_button').val(text);
+ 
+	}
 
 function cart_get_number_of_items()
 {
@@ -36,4 +51,20 @@ function cart_get_number_of_items()
 			}
 	}
 	return cnt;
+}
+
+function cart_get_orders()
+{
+	var orders = 0
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+			var key = window.localStorage.key(i); // получаем ключ
+			var value = window.localStorage.getItem(key); // получаем значение по ключу
+
+			if (key.indexOf('product_') == 0) 
+			{
+					orders = orders + key + '=' + value +',';
+			}
+	}
+	return orders;
 }
